ebdb2c8d9bc3cb7c5b0f5f4ae4adeac7317d9681
Moziful Haque <mhmoni2310@gmail.com>
2026-03-09 00:49:29 +0600

    Successfully Redirect to Issues Page If Credentials Are Correct

--- signInScript.js
+++ signInScript.js
@@ -3,7 +3,11 @@ document.getElementById("signInButton").addEventListener("click", ()=>{
     const EnteredPassword = document.getElementById("passwordInput").value;
     if(EnteredUsername === "admin" && EnteredPassword === "admin123"){
         console.log("Correct Username & Password");
+        document.getElementById("signInContainer").classList.add("hidden");
+        document.getElementById("loader").classList.remove("hidden");
+        window.location.assign("./issues.html");
     } else {
-        alert("Wrong Credentials!")
+        alert("Wrong Credentials!");
+        return;
     }
 });
